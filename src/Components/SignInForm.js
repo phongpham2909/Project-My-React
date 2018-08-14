ed3972351f373a0d796c621070473a85450669da
phongpham2909 <phongpham2140051@gmail.com>
2018-08-14 17:55:41 +0700

    Update File

--- src/Components/SignInForm.js
+++ src/Components/SignInForm.js
@@ -6,8 +6,9 @@ class SignInForm extends Component {
     {
         super();
         this.state = {
-            email: '',
-            password: ''
+            message: "Username or Password error!!!",
+            data:{ email: '',  password: ''}
+           
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,18 +27,25 @@ class SignInForm extends Component {
         event.preventDefault();
         console.log(this.state);
     }
+    handleKeyPress(event)
+    {
+        if(event.keyCode === 13)
+        {
+
+        }
+    } 
     render() {
       return (
           <div className="FormCenter">
               <form onSubmit={this.handleSubmit} className="FormFields">
                   <div className="FormField">
                       <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                      <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange}/>
+                      <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.data.email} onChange={this.handleChange}/>
                   </div>
 
                   <div className="FormField">
                       <label className="FormField__Label" htmlFor="password">Password</label>
-                      <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"  value={this.state.password} onChange={this.handleChange} />
+                      <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"  value={this.state.data.password} onChange={this.handleChange} />
                   </div>
                   <div className="FormField">
                       <button className="FormField__Button mr-20">Sign In</button> <Link to="/sign-up" className="FormField__Link">Create an account</Link>
